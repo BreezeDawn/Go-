cee0e4231852bc0e7d5e44547d405292fca1bef6
Breezedawn <BreezeGent@gmail.com>
2019-06-07 11:12:28 +0800

    第二天（二）

--- src/two_go/go_channel.go
+++ src/two_go/go_channel.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func go_channel() {
+func goChannel() {
 	pipe := make(chan int, 3)
 	pipe <- 1
 	pipe <- 2
@@ -12,4 +15,13 @@ func go_channel() {
 	pipe <- 4
 	fmt.Println(p1)
 	fmt.Println(len(pipe))
+}
+
+func goChannelAdd(a int, b int, pipe chan int) {
+	pipe <- a + b
+	time.Sleep(5 * time.Second)
+}
+
+func goReturnMany() (int, int, int) {
+	return 1, 2, 3
 }
--- src/two_go/main.go
+++ src/two_go/main.go
@@ -1,6 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 	// 1. channel
-	go_channel()
+	goChannel()
+
+	// 2. channel add
+	pipe := make(chan int, 1)
+	go goChannelAdd(1, 2, pipe)
+	addRes := <-pipe
+	fmt.Println(addRes)
+
+	// 3. func return many
+	a, b, c := goReturnMany()
+	fmt.Println(a, b, c)
 }
